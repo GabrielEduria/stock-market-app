8cb012f23adf45a6a9bc59710e986d053d412054
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-11 16:06:35 +0800

    add

--- hooks/useTradingViewWidget.tsx
+++ hooks/useTradingViewWidget.tsx
@@ -1,9 +1,24 @@
 import { useEffect, useRef,} from "react";
 
-const useTradingViewWidget = () => {
-  return (
-    <div>useTradingViewWidget</div>
-  )
+const useTradingViewWidget = (scriptURL: string, config: Record<string, unknown>, height = 600) => {
+   const containerRef = useRef<HTMLDivElement | null>(null);
+   
+   useEffect(
+    if(!containerRef.current) return;
+    if(containerRef.current.dataset.loaded) return;
+    containerRef.current.innerHTML = `<div> </div>`
+       () => {
+         const script = document.createElement("script");
+         script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+         script.type = "text/javascript";
+         script.async = true;
+         script.innerHTML = ``;
+         container.current.appendChild(script);
+       },
+       []
+     );
+
+   return containerRef;
 }
 
 export default useTradingViewWidget
