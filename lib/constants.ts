32724b17cd942d63a1064c7e15a59c77b2d801df
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-11 15:47:58 +0800

    removed line 49

--- lib/constants.ts
+++ lib/constants.ts
@@ -45,8 +45,8 @@ export const MARKET_OVERVIEW_WIDGET_CONFIG = {
     showFloatingTooltip: true, 
     plotLineColorGrowing: '#0FEDBE',
     plotLineColorFalling: '#0FEDBE', 
-    gridLineColor: 'rgba(240, 243, 250, 0)', // grid line color
-    scaleFontColor: '#DBDBDB', // font color for scale
+    gridLineColor: 'rgba(240, 243, 250, 0)', 
+    scaleFontColor: '#DBDBDB', 
     belowLineFillColorGrowing: 'rgba(41, 98, 255, 0.12)', // fill under line when growing
     belowLineFillColorFalling: 'rgba(41, 98, 255, 0.12)', // fill under line when falling
     belowLineFillColorGrowingBottom: 'rgba(41, 98, 255, 0)',
