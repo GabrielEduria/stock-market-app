42311b650335c43ce8a31131835154744cd82197
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-12 01:11:46 +0800

    component renamed

--- components/charts/TradingViewWidget.tsx
+++ components/charts/TradingViewWidget.tsx
@@ -14,12 +14,12 @@ interface TradingViewWidgetProps {
 }
 
 function TradingViewWidget({ title, scriptURL, config, height = 600, className }: TradingViewWidgetProps) {
-  const container = useTradingViewWidget( scriptURL, config, height );
+  const containerRef = useTradingViewWidget( scriptURL, config, height );
 
 
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+    <div className="tradingview-widget-container" ref={containerRef} style={{ height: "100%", width: "100%" }}>
       <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
       <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
     </div>
