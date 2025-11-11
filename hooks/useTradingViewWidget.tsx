8afd98e1779f9bec230766b74b27b26eae4ba525
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-11 17:02:55 +0800

    .

--- hooks/useTradingViewWidget.tsx
+++ hooks/useTradingViewWidget.tsx
@@ -3,11 +3,12 @@ import { useEffect, useRef,} from "react";
 const useTradingViewWidget = (scriptURL: string, config: Record<string, unknown>, height = 600) => {
    const containerRef = useRef<HTMLDivElement | null>(null);
    
-   useEffect(
-    if(!containerRef.current) return;
-    if(containerRef.current.dataset.loaded) return;
-    containerRef.current.innerHTML = `<div class="tradingview-widget-container__widget" style="width: 100%; height: ${height}px;"></div>`
-       () => {
+   useEffect(() => {
+     if(!containerRef.current) return;
+     if(containerRef.current.dataset.loaded) return;
+     containerRef.current.innerHTML = `<div class="tradingview-widget-container__widget" style="width: 100%; height: ${height}px;"></div>`;
+
+     
          const script = document.createElement("script");
          script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
          script.type = "text/javascript";
