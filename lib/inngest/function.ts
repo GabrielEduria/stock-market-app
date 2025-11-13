1ad194cdc7b54af608bbc0f5e93bd7dd134e2679
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-13 15:09:16 +0800

    changed some comments

--- lib/inngest/function.ts
+++ lib/inngest/function.ts
@@ -1,4 +1,6 @@
+import { success } from "better-auth/*"
 import { inngest } from "./client"
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts"
 
 export const sendSignUpEmail = inngest.createFunction(
     { id: 'sign-up-email' },
@@ -10,5 +12,34 @@ export const sendSignUpEmail = inngest.createFunction(
             - Risk tolerance: ${event.data.riskTolerance}
             - Preferred industry: ${event.data.preferredIndustry}
         `
+
+
+        const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace('{{userProfile}}', userProfile)
+
+        const response = await step.ai.infer('generate-welcome-intro', {
+            model: step.ai.models.gemini({ model: 'gemini-2.5-flash-lite' }), 
+            body: {
+                contents: [
+                    {
+                        role: 'user',
+                        parts: [
+                            {text: prompt }
+                        ]
+                    }
+                ]
+            }
+        })
+
+        await step.run('send-welcome-email', async () => {
+            const part = response.candidates?.[0]?.content?.parts?.[0];
+            const introText = (part && 'text' in part ? part.text : null) || 'Thanks for joining Stock Market by Gabbbyyyy. You now have the tools to track market and make smarter moves';
+
+            //!EMAIL sending logic
+        })
+
+        return {
+            success: true,
+            message: 'Welcome email sent successfully'
+        }
     }
 )
