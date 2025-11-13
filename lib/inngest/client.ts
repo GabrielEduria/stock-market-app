b24ba5148736176b5cabc5521206fcb12ddf2808
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-13 20:27:11 +0800

    client.ts !

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,5 +2,5 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
     id: 'stock-market-app',
-    ai: { gemini: { apiKey: process.env.GEMINI_API_KEY }}
+    ai: { gemini: { apiKey: process.env.GEMINI_API_KEY! }}
 })
