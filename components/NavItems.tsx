aab9325e97ba2cde54c6bab58175b839a88c5842
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-14 20:26:16 +0800

    update

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,6 +2,7 @@
 import { NAV_ITEMS } from "@/lib/constants"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchCommand from "./SearchCommand";
 
 const NavItems = () => {
   const pathname: string = usePathname();
@@ -15,15 +16,25 @@ const NavItems = () => {
 
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-        {NAV_ITEMS.map(({ href, label }) => (
-            <li key={href}>
+        {NAV_ITEMS.map(({ href, label }) => {
+
+          if(label === 'Search') return (
+            <li key="search-trigger">
+              <SearchCommand 
+                label="Search"
+                renderAs="text" 
+                initialStocks={[]}  
+              />
+            </li>
+          )
+          return <li key={href}>
                <Link href={href} className={`hover:text-yellow-500 transition-colors ${
                   isActive(href) ? 'text-gray-100' : ''
                }`}>
                   {label}
                </Link>
             </li>
-        ))}
+    })}
     </ul>
   )
 }
