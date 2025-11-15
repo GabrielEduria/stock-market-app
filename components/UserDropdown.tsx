c5c20ada989d414368719e8a6f87f58756c2260c
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-15 15:26:35 +0800

    more issue

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -21,7 +21,7 @@ const UserDropdown = () => {
     router.push("/sign-in");
   }
 
-  const user = { name: 'Gab', email: 'gabrielezekieleduria@gmail.com' };
+  const user = { name: 'Gab', email: 'projectbygab@gmail.com' };
 
   return (
         <DropdownMenu>
@@ -60,10 +60,13 @@ const UserDropdown = () => {
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator className="bg-gray-600" />
-              <DropdownMenuItem onClick={handleSignOut}  className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+
+              {/* COMMENT OUT WHILE FIXING */}
+              {/* <DropdownMenuItem onClick={handleSignOut}  className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
                  Logout
-              </DropdownMenuItem>
+              </DropdownMenuItem> */}
+              
               <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
               <nav className="sm:hidden">
                  <NavItems />
