14e4d1d5a43b26f3fb1e49b88bcf52a90cbcf69a
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-15 15:29:59 +0800

    commentted out

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -69,7 +69,7 @@ const UserDropdown = () => {
               
               <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
               <nav className="sm:hidden">
-                 <NavItems />
+                 <NavItems initialStocks={[]} />
               </nav>
         </DropdownMenuContent>
       </DropdownMenu>
