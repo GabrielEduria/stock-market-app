5f55d75aae8fe2c1d073c0465dee0e2cf0d5d53b
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-16 14:08:18 +0800

    update

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -43,7 +43,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="projectbygabbbyyyy@jsmastery.com"
+                    placeholder="projectbygabbbyyyy@gmail.com"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
