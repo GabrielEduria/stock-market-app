7d381f6c5b90af91d1170a1529b5c7a3a9593243
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-16 13:51:46 +0800

    fixed

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,10 +4,10 @@ import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
-import {signInWithEmail, signUpWithEmail} from "@/lib/actions/auth.actions";
-import {toast} from "sonner";
-import {signInEmail} from "better-auth/api";
-import {useRouter} from "next/navigation";
+import { signInWithEmail, signUpWithEmail } from "@/lib/actions/auth.actions";
+import { toast } from "sonner";
+import { signInEmail } from "better-auth/api";
+import { useRouter } from "next/navigation";
 
 const SignIn = () => {
     const router = useRouter()
@@ -43,7 +43,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@jsmastery.com"
+                    placeholder="projectbygabbbyyyy@jsmastery.com"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
-import NavItems from "./NavItems";
-import UserDropdown from "./UserDropdown";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+import {searchStocks} from "@/lib/actions/finnhub.actions";
 
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks();
 
-const Header = () => {
-  return (
-    <header className="sticky top-0 header"> 
-      <div className="container header-wrapper">
-        <Link href="/">
-           <Image 
-            src="/public/assets/icons/logo.svg" 
-            alt="Signalist logo" 
-            width={200}
-            height={50} 
-            className="h-10 w-auto cursor-pointer" 
-           />
-        </Link>
-         <nav className="hidden sm:block">
-              <NavItems initialStocks={[]} />
-        </nav>
-        <UserDropdown />
-      </div>
-    </header>
-  )
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+          <Link href="/">
+            <Image 
+              src="/public/assets/icons/logo.svg" 
+              alt="Signalist logo" 
+              width={200}
+              height={50} 
+              className="h-8 w-auto cursor-pointer" 
+            />
+          </Link>
+                <nav className="hidden sm:block">
+                    <NavItems initialStocks={initialStocks} />
+                </nav>
+
+                <UserDropdown user={user} initialStocks={initialStocks} />
+            </div>
+        </header>
+    )
 }
-
 export default Header;
+
+
+
+
